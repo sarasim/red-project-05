6ecf7a2434a16e701bc8b90db6230a970b3303aa
Sara Sim <sarasim@Saras-MacBook-Air.local>
2016-03-13 17:22:21 -0700

    Edit details of About page.

--- wp-content/themes/leredbread/about.php
+++ wp-content/themes/leredbread/about.php
@@ -1,31 +1,28 @@
 <?php
 /**
- * Template Name: About Page
+ * The template for the about page.
+ *
+ * @package RED_Starter_Theme
  */
 
  get_header(); ?>
 
  <div id="primary" class="content-area">
    <main id="main" class="site-main" role="main">
-
-    <div class="about-page container">
-
-      <header class="about-header">
+     <div class="about-page container">
+      <section class="about-header">
        <h1>Learn About Our Team and Culture</h1>
         <p><?php echo CFS()->get( 'tagline' ); ?></p>
         <hr class="decorative"></hr>
-      </header>
-
+      </section>
       <div class="entry-content">
-
         <div class="about-info">
           <div class="about-info-item">
-             <img src="<?php bloginfo('template_directory'); ?>/images/team.jpg" alt="Le Red Bread Team" />
+            <img src="<?php bloginfo('template_directory'); ?>/images/team.jpg" alt="Le Red Bread Team" />
             <h3>Le Red Bread Team</h3>
             <span class="sub-header">Baking up a storm every day.</span>
             <p><?php echo CFS()->get( 'team_copy' ); ?></p>
           </div>
-
           <div class="about-info-item">
             <img src="<?php bloginfo('template_directory'); ?>/images/bakery.jpg" alt="Le Red Bread Bakery" />
             <h3>Le Red Bread Bakery</h3>
@@ -33,15 +30,16 @@
             <p><?php echo CFS()->get( 'bakery_copy' ); ?></p>
           </div>
         </div>
-
         <div class="our-story">
           <h2>Our Story</h2>
           <p><?php echo CFS()->get( 'our_story_copy' ); ?></p>
         </div>
       </div>
-      <div class="click-for-more container">
-        <p>Feel free to contact us with any questions coments or suggestions. We even take custom orders!</p>
-          <button><a href="<?php echo get_term_link( $term ); ?>">Contact Us</a></button>
+      <div class="contact-us container clearfix">
+        <p><span>Feel free to contact us with any questions coments or suggestions. We even take custom orders!</span>
+          <a href="<?php bloginfo('template_directory'); ?>/contact" class="cta-btn">Contact Us</a>
+        </p>
+      </div>
     </div>
   </main>
  </div>
